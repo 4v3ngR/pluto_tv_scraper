0a9338d5b6356e724a2bd745153fe6a42ada932d
4v3ngR <avenger@rescueteam.com>
2024-01-15 09:11:35 +0800

    more code cleanup

--- index.js
+++ index.js
@@ -2,50 +2,9 @@
 
 (async function() {
 	const fs = require('fs');
-	const plutoapi = require('./lib/plutotv/api');
-	const utils = require('./lib/utils');
 	const config = require('./lib/config');
-
-	const regionalPlaylists = {};
-	const regionalEpgs = {};
+	const plutotv = require('./lib/plutotv');
 
 	config.loadConfig();
-
-	const mapping = config.getMapping();
-	const group = config.get('group');
-	const regionalize = config.get('regionalize');
-	const all = config.get('all');
-	const outdir = config.get('outdir');
-
-	const processRegion = async (region) => {
-		console.info("INFO: processing", region);
-		try {
-			const clientID = config.get('clientID');
-			const xff = mapping[region];
-
-			const bootData = await plutoapi.boot(xff, clientID);
-			const channels = await plutoapi.channels(xff);
-			const categories = await plutoapi.categories(xff);
-			const timelines = await plutoapi.timelines(xff);
-
-			const m3u8 = await plutoapi.generateM3U8(region, group, regionalize);
-			const xmltv = await plutoapi.generateXMLTV(region, regionalize);
-			fs.writeFileSync(`${outdir}/plutotv_${region}.m3u8`, m3u8, 'utf-8');
-			fs.writeFileSync(`${outdir}/plutotv_${region}.xml`, xmltv, 'utf-8');
-
-			regionalPlaylists[region] = m3u8;
-			regionalEpgs[region] = xmltv;
-		} catch (ex) {
-			console.error("ERROR: got exception", ex.message);
-		}
-	}
-
-	for (const key of Object.keys(mapping)) await processRegion(key);
-
-	if (all && Object.keys(mapping).length > 1) {
-		const m3u8 = utils.mergeM3U8(regionalPlaylists);
-		const xmltv = utils.mergeXMLTV(regionalEpgs);
-		fs.writeFileSync(`${outdir}/plutotv_all.m3u8`, m3u8, 'utf-8');
-		fs.writeFileSync(`${outdir}/plutotv_all.xml`, xmltv, 'utf-8');
-	}
+	await plutotv.process(config);
 })();
