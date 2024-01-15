456faca43a758a918323f6c70f7b1a98219d0e8c
4v3ngR <avenger@rescueteam.com>
2024-01-15 08:58:57 +0800

    move all option related stuff to config.json

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const fs = require('fs');
-const plutoapi = require('./lib/plutotv/api');
-const utils = require('./lib/utils');
-const config = require('./lib/config');
-
 (async function() {
-	const options = program
-		.option('--config <configfile>', 'provide the location to the configuration file')
-		.option('--mapping <region,IP>', 'provide a region and IP address to process instead of the mapping')
-		.option('--outdir <outdir>', 'provide the destination directory')
-		.option('--clientid <clientid>', 'provide a client id')
-		.option('--all', 'merge all regions into a single playlist and epg')
-		.option('--group [genre|country]', 'specify the grouping in the playlist')
-		.option('--regionalize', 'append the country code to the channel id')
-		.option('-h --help', 'display the help')
-		.parse(process.argv)
-		.opts();
-
-	if (options.help) {
-		utils.displayHelp();
-		process.exit(0);
-	}
-
-	config.loadConfig(options);
-
-	const mapping = config.getMapping();
+	const fs = require('fs');
+	const plutoapi = require('./lib/plutotv/api');
+	const utils = require('./lib/utils');
+	const config = require('./lib/config');
 
 	const regionalPlaylists = {};
 	const regionalEpgs = {};
 
+	config.loadConfig();
+
+	const mapping = config.getMapping();
 	const group = config.get('group');
 	const regionalize = config.get('regionalize');
 	const all = config.get('all');
 	const outdir = config.get('outdir');
 
-	const get = async (region) => {
+	const processRegion = async (region) => {
 		console.info("INFO: processing", region);
 		try {
 			const clientID = config.get('clientID');
@@ -59,7 +40,7 @@ const config = require('./lib/config');
 		}
 	}
 
-	for (const key of Object.keys(mapping)) await get(key);
+	for (const key of Object.keys(mapping)) await processRegion(key);
 
 	if (all && Object.keys(mapping).length > 1) {
 		const m3u8 = utils.mergeM3U8(regionalPlaylists);
